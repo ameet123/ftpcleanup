447dc53059cc2dc123f0b7a7dcd931e980d8fd8c
Ameet Chaubal <ameet.chaubal@ihg.com>
2017-09-06 16:18:00 -0400

    added cmd line options

--- src/main/java/com/ihg/hapi/holidex/CleanupApp.java
+++ src/main/java/com/ihg/hapi/holidex/CleanupApp.java
@@ -2,6 +2,7 @@ package com.ihg.hapi.holidex;
 
 import com.google.common.base.Stopwatch;
 import com.google.common.base.Strings;
+import org.apache.commons.cli.*;
 import org.apache.commons.text.RandomStringGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +32,10 @@ public class CleanupApp {
     private static RandomStringGenerator generator;
     private Pattern dateRegex;
     private Date startDate, endDate;
+    private String sourceDir, targetDir;
+    private int cnt;
+    private String fileDate;
+    private String deleteDirectory;
 
     /**
      * accept date strings and convert them to date type
@@ -53,6 +58,69 @@ public class CleanupApp {
     }
 
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * int cnt, String date, String dirName, String targetDir) {
+     *
+     * @param args
+     */
+    private void parseCmdOptions(String[] args) {
+        Options options = new Options();
+
+        Option generate = new Option("g", "generate", false, "generate test data");
+        generate.setRequired(false);
+        options.addOption(generate);
+        Option generateCnt = new Option("c", "count", true, "number of files to generate");
+        generateCnt.setRequired(false);
+        options.addOption(generateCnt);
+        Option generateDate = new Option("dt", "date", true, "date for files");
+        generateDate.setRequired(false);
+        options.addOption(generateDate);
+        Option generateSourceDir = new Option("src", "sourceDir", true, "source directory");
+        generateSourceDir.setRequired(false);
+        options.addOption(generateSourceDir);
+        Option generateTargetDir = new Option("tgt", "targetDir", true, "target directory");
+        generateTargetDir.setRequired(false);
+        options.addOption(generateTargetDir);
+
+        Option delete = new Option("d", "delete", false, "delete files from dir");
+        delete.setRequired(false);
+        options.addOption(delete);
+        Option deleteDir = new Option("dd", "deleteDir", true, "directory to delete");
+        deleteDir.setRequired(false);
+        options.addOption(deleteDir);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+
+            String count = cmd.getOptionValue("count");
+            fileDate = cmd.getOptionValue("date");
+            sourceDir = cmd.getOptionValue("sourceDir");
+            targetDir = cmd.getOptionValue("targetDir");
+            if (!Strings.isNullOrEmpty(count)) {
+                cnt = Integer.parseInt(count);
+            }
+
+            // delete options
+            deleteDirectory = cmd.getOptionValue("deleteDir");
+
+        } catch (org.apache.commons.cli.ParseException e) {
+            LOGGER.error("Err: parsing options", e);
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+        }
+
+        String inputFilePath = cmd.getOptionValue("input");
+        String outputFilePath = cmd.getOptionValue("output");
+
+        System.out.println(inputFilePath);
+        System.out.println(outputFilePath);
 
     }
 
